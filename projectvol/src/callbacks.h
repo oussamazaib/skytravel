4fa1b7956bfc35e9b6b5d883da185a92b34dccd9
oussamazaib <oussama.zaibi@esprit.tn>
2019-12-11 19:05:38 -0800

    first

---
+++ projectvol/src/callbacks.h
@@ -0,0 +1,105 @@
+#include <gtk/gtk.h>
+
+
+void
+on_ajouter_clicked                     (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_afficher_clicked                     (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void on_retour_clicked (GtkWidget *objet, gpointer user_data);
+
+
+
+void
+on_modifier_clicked                    (GtkWidget       *objet,
+                                        gpointer         user_data);
+/*
+void
+on_confirmer_clicked                   (GtkWidget      *objet,
+                                        gpointer         user_data);*/
+
+void
+on_ajout_clicked                       (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_supprimer_clicked                   (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_supprr_clicked                      (GtkWidget      *objet,
+                                        gpointer         user_data);
+
+void
+on_modifr_clicked                      (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_button4return_clicked               (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_retour1enterid_clicked              (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_button1gestionvol_clicked           (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_modifyvol1_clicked                  (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_buttonconfirm_clicked               (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_button5_clicked                     (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_button9hotel_clicked                (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button6hotel_clicked                (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button7hotel_clicked                (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button8hotel_clicked                (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button13hotel_clicked               (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button12hotel_clicked               (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_button2hotel_clicked                (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button15hotel_clicked               (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button13hotel_clicked               (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button13shotel_clicked              (GtkButton       *button,
+                                        gpointer         user_data);
+
+
+
